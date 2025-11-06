a6a9316fe8936bd93f7bcc094a3f10222b0b36e6
amer-devops <amerahmed.official@gmail.com>
2025-11-06 19:16:29 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -24,13 +24,13 @@ resource "terraform_data" "mongodb" {
     host     = aws_instance.mongodb.private_ip
   }
 
-    # terraform copies this file to mongodb server
+  # terraform copies this file to mongodb server
   provisioner "file" {
     source = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
   }
 
-    provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
         # "sudo sh /tmp/bootstrap.sh"
@@ -38,5 +38,3 @@ resource "terraform_data" "mongodb" {
     ]
   }
 }
-
-
