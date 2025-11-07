30360f31ee3336d4097cb54111c307cc55a1f089
amer-devops <amerahmed.official@gmail.com>
2025-11-07 16:41:16 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+component=$1
+environment=$2
 dnf install ansible -y
-ansible-pull -U https://github.com/Amer-devops/ansible-roboshop-roles-tf.git -i inventory.ini -e component=mongodb main.yaml
+ansible-pull -U https://github.com/Amer-devops/ansible-roboshop-roles-tf.git -i inventory.ini -e component=$component main.yaml
