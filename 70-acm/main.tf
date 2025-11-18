40e28c424964316b1485c43e36393f24ddbc77df
amer-devops <amerahmed.official@gmail.com>
2025-11-18 13:16:01 +0530

    terraform

---
+++ 70-acm/main.tf
@@ -0,0 +1,39 @@
+# Creating certificate
+resource "aws_acm_certificate" "roboshop" {
+  domain_name       = "*.${var.domain_name}"
+  validation_method = "DNS"
+
+  tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-mongodb" # roboshop-dev-mongodb
+        }
+    )
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# Creating Route-53-record for certificate validation
+
+resource "aws_route53_record" "roboshop" {
+  for_each = {
+    for dvo in aws_acm_certificate.roboshop : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 1
+  type            = each.value.type
+  zone_id         = var.zone_id
+}
+
+# aws_acm_certificate_validation
+resource "aws_acm_certificate_validation" "roboshop" {
+  certificate_arn         = aws_acm_certificate.roboshop.arn
+  validation_record_fqdns = [for record in aws_route53_record.roboshop : record.fqdn]
+}
---
+++ 70-acm/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+    default = "roboshop"  
+}
+
+variable "environment" {
+    default = "dev"  
+}
+variable "domain_name" {
+    default = "daws86s.icu"
+}
+
+variable "zone_id" {
+    default = "Z09921652HC7E9W0W018V"
+}
